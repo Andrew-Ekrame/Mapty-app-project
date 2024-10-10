bd5467a06aef734ad9fd75cb808221f0d7884c3e
Andrew-Ekrame <andrewekrame2@gmail.com>
2024-10-10 11:22:27 +0300

    displaying map marker

--- script.js
+++ script.js
@@ -30,6 +30,22 @@ if (navigator.geolocation) {
         .addTo(map)
         .bindPopup("A pretty CSS popup.<br> Easily customizable.")
         .openPopup();
+      map.on("click", function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          ).setPopupContent("workout")
+          .openPopup();
+      });
     },
     function () {
       alert("couldn't get position");
